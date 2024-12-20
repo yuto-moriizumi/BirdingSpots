5535bed05bf33e29fc3189bfae37b89171adf0ce
volgakurvar <kurvan1112@gmail.com>
2024-12-20 23:26:44 +0900

    DynamoDBからPostgresに移行

--- src/addSpot.ts
+++ src/addSpot.ts
@@ -1,14 +1,20 @@
 "use server";
 
+import { PrismaClient } from "@prisma/client";
 import { Spot } from "./model/Spot";
-import { SpotModel } from "./model/SpotModel";
+
+const prisma = new PrismaClient();
 
 export const addSpot = async (spotData: Spot) => {
   try {
-    await new SpotModel(spotData).save();
+    await prisma.spot.create({
+      data: spotData,
+    });
     return true;
   } catch (error) {
     console.error("Error adding spot:", error);
     return false;
+  } finally {
+    await prisma.$disconnect();
   }
 };
